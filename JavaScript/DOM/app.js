3298cdae354956e3db65494e6fa971dc56d3d83e
kunj-pandya <kunjpandya998@gmail.com>
2025-02-27 19:21:17 +0530

    DOM Update

---
+++ JavaScript/DOM/app.js
@@ -0,0 +1,45 @@
+
+// selecting using class name. 
+// Return the element as an HTML collection or empty collection(if not found).
+
+// let smallImages = document.getElementsByClassName("oldImg");
+// for (let i = 0; i < smallImages.length; i++){
+//     console.dir(smallImages[i]);
+//     console.dir(smallImages[i].src);
+// }
+
+// changing images using Class name.
+
+// let smallImages = document.getElementsByClassName("oldImg");
+// for (let i = 0; i < smallImages.length; i++){
+//     smallImages[i].src = "assets/spiderman_img.png";
+//     console.log(`value of image no. ${i} is changed.`);
+    // }
+
+
+//  Query Selector 
+//  Query Selector : Allows us use any CSS seletor.
+
+// document.querySelector("p"); 
+//  selects first p element
+
+// document.querySelector("#myId"); 
+//  selects first element with id = myId.
+
+// document.querySelector(".myClass"); 
+//  selects first element with class = myClass.
+
+// document.querySelectorAll("p");
+// selects all p elements.
+
+
+// console.dir(document.querySelector("h1"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".oldImg"));
+
+//  Selects all elements with class = oldImg
+//  console.dir(document.querySelectorAll(".oldImg"));
+
+
+// console.dir(document.querySelector("div a"));
+
